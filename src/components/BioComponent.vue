ff69297baae205ff7c4e82b421e61353c50a915d
AlessandroMungai <mungai.alessandro90@gmail.com>
2024-02-05 17:25:25 +0100

    commit

--- src/components/BioComponent.vue
+++ src/components/BioComponent.vue
@@ -13,78 +13,85 @@ export default {
 
 <template>
     <div class="bio-section">
-        <div class="d-flex my-4 align-items-center">
-            <div class="col-auto">
-                <img src="../../public/me.png" class="avatar" alt="">
-            </div>
-            <div class="col-auto">
-                <h2>
-                    Ciao!
-                </h2>
+        <div class="d-flex align-items-center cit-section">
+            <div class="col-12 align-items-center text-center my-4">
+                <img src="../../public/grafica_portfolio_daje.png" class="avatar" alt="">
             </div>
         </div>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil accusamus,
-            hic ullam sapiente laudantium natus a quas possimus. Ducimus porro eum enim
-            unde in, commodi sunt consectetur quam minus ipsam atque numquam voluptas.
-            Vitae veniam eligendi consequuntur laborum esse illum amet
-            , assumenda quas totam, dolores tenetur blanditiis fugit, officia atque?
-            Corrupti, quisquam. Nostrum culpa exercitationem dolorem, minus ab tempora 
-            placeat velit error voluptate, sunt libero sint, a suscipit obcaecati? Harum 
-            aut dicta voluptatibus est autem, fuga nihil assumenda praesentium iusto? Alias 
-            ab quos aliquam excepturi saepe minus accusantium, eos ducimus similique culpa consequatur,
-            <br>
-            <br>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil accusamus,
-            hic ullam sapiente laudantium natus a quas possimus. Ducimus porro eum enim
-            unde in, commodi sunt consectetur quam minus ipsam atque numquam voluptas.
-            Vitae veniam eligendi consequuntur laborum esse illum amet
-            , assumenda quas totam, dolores tenetur blanditiis fugit, officia atque?
-            Corrupti, quisquam. Nostrum culpa exercitationem dolorem, minus ab tempora 
-            placeat velit error voluptate, sunt libero sint, a suscipit obcaecati? Harum 
-            aut dicta voluptatibus est autem, fuga nihil assumenda praesentium iusto? Alias 
-            ab quos aliquam excepturi saepe minus accusantium, eos ducimus similique culpa consequatur,
-            <br>
-            <br>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil accusamus,
-            hic ullam sapiente laudantium natus a quas possimus. Ducimus porro eum enim
-            unde in, commodi sunt consectetur quam minus ipsam atque numquam voluptas.
-            Vitae veniam eligendi consequuntur laborum esse illum amet
-            , assumenda quas totam, dolores tenetur blanditiis fugit, officia atque?
-            Corrupti, quisquam. Nostrum culpa exercitationem dolorem, minus ab tempora 
-            placeat velit error voluptate, sunt libero sint, a suscipit obcaecati? Harum 
-            aut dicta voluptatibus est autem, fuga nihil assumenda praesentium iusto? Alias 
-            ab quos aliquam excepturi saepe minus accusantium, eos ducimus similique culpa consequatur,
-            <br>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil accusamus,
-            hic ullam sapiente laudantium natus a quas possimus. Ducimus porro eum enim
-            unde in, commodi sunt consectetur quam minus ipsam atque numquam voluptas.
-            Vitae veniam eligendi consequuntur laborum esse illum amet
-            , assumenda quas totam, dolores tenetur blanditiis fugit, officia atque?
-            Corrupti, quisquam. Nostrum culpa exercitationem dolorem, minus ab tempora 
-            placeat velit error voluptate, sunt libero sint, a suscipit obcaecati? Harum 
-            aut dicta voluptatibus est autem, fuga nihil assumenda praesentium iusto? Alias 
-            ab quos aliquam excepturi saepe minus accusantium, eos ducimus similique culpa consequatur,
-        </p>
+        <div class="bio-text">
+            <div class="container py-3">
+                <div class="row d-flex align-items-stretch justify-content-between">
+                    <div class="col-6">
+                        <div class="card card-style">
+                            <h2 class="text-white my-4">
+                                Hey!
+                            </h2>
+                            <h4 class="text-white">
+                                Benvenuto nel mio Portfolio!.
+                            </h4> 
+                            <p class="text-justify">
+                                Mi chiamo Alessandro Mungai e sono un Junior Frontend Developer con la passione per la Grafica.
+                                Dopo aver conseguito il diploma, ho sempre svolto attività lavorative d'ufficio poco creative. Al termine
+                                dell’ultimo percorso aziendale, ho deciso di rimettermi in gioco conseguendo dapprima un master come
+                                Graphic Designer e successivamente un master come Fullstack Web Developer. La fusione di queste
+                                due formazioni mi ha permesso di unire la mia passione per il design grafico alla competenza tecnica nel
+                                campo dello sviluppo web, consentendomi cosi di trasformare le mie idee creative in applicazioni interattive.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex align-self-stretch">
+                        <div class="card card-style">
+                            <h2 class="text-white my-4">
+                                Le tecnologie che uso
+                            </h2>
+                            <p>
+                                Nel corso del mio percorso di studi e formazione, ho approfondito le tecnologie per la grafica e lo sviluppo, acquisendo competenze significative in ambiti come:
+                            </p>
+                            <img src="../../public/icone lavoro.png" alt="" class="img-tec" srcset="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/main";
 .bio-section {
+    background: rgb(0,0,0);
+    background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(61,60,60,1) 100%); 
     img {
-        width: 90px;
+        width: 700px;
+        position: relative;
+        z-index: 1000;
+        margin-top: 80px;
+        margin-bottom: 80px;
     }
 
-    padding: 100px;
-    h2 {
-        font-size: 3.5rem;
-        margin:20px 0;
-        color: white;
-        margin-left: 15px;
+    .card-style {
+        background-color: rgb(30, 30, 30);
+        padding: 20px;
+        transition: all 0.3s ease-in-out;
+        
+        &:hover {
+            background-color: rgb(24, 24, 24)
+        }
+    }
+    .img-tec {
+        width: 300px;
+        margin-top: 10px;
     }
     p {
         color: white;
     }
+
+    h2 {
+        font-size: 2.5rem;
+    }
+    .bio-text {
+        width: 100vw;
+    }
+
 }
 </style>
